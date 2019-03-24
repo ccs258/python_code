ce7202a17f7131899735671613935ad2adcccf35
ccs <1154911548@qq.com>
2019-03-24 22:47:42 +0800

    xuexi

--- credits_predict.py
+++ credits_predict.py
@@ -92,3 +92,65 @@ def print_Kfold_scores(X_train_data,y_train_data):
     return best_c
 
 best_c = print_Kfold_scores(X_train_undersample, y_train_undersample)
+import itertools
+
+def plot_confusion_matrix(cm,classes,normalize=False,title='confusion matrix',cmap=plt.cm.Blues):
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks,classes,rotation=0)
+    plt.yticks(tick_marks,classes)
+    if normalize:
+        cm = cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+    else:
+        1
+    thresh = cm.max()/2
+    for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i,cm[i,j],horizontalalignment='center',color='white' if cm[i,j]>thresh else 'black')
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+
+lr = LogisticRegression(C = best_c,penalty='l1')
+lr.fit(X_train_undersample,y_train_undersample.values.ravel())
+y_pred_undersample = lr.predict(X_test_undersample.values)
+
+cnf_matrix = confusion_matrix(y_test_undersample,y_pred_undersample)
+np.set_printoptions(precision=2)
+print("Recall metric in the testing dataset: ",cnf_matrix[1,1]/(cnf_matrix[1,0]+cnf_matrix[1,1]))
+class_names =[0,1]
+plt.figure()
+plot_confusion_matrix(cnf_matrix,classes=class_names,title='Confusion matrix')
+plt.show()
+
+
+lr = LogisticRegression(C=best_c,penalty='l1')
+y_pred_undersample_score = lr.fit(X_train_undersample,y_train_undersample.values.ravel())\
+    .decision_function(X_test_undersample.values)
+
+fpr,tpr,thresholds = roc_curve(y_test_undersample.values.ravel(),
+                               y_pred_undersample_score)
+
+roc_auc = auc(fpr,tpr)
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr,tpr,label='AUC = %.2f'% roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0,1],[0,1],'r--')
+plt.xlim([-0.1,1.0])
+plt.ylim([-0.1,1.01])
+plt.ylabel('True Positive Rate')
+plt.xlabel('Fasle Positive Rate')
+plt.show()
+
+precision,recall,threshold = precision_recall_curve(y_test_undersample.values.ravel(),
+                                                   y_pred_undersample_score)
+plt.title('Precision-Recall Curve')
+plt.plot(recall,precision)
+plt.xlim([0.0,1.10])
+plt.ylim([0.0,1.10])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.show()
